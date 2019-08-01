ea5101485fcfbd7e270584a4e5f10ebfccaee575
akbarsyamisid <akbar.syam@isid.co.jp>
2019-08-01 18:12:18 +0900

    Added new enemy - UnityChan

--- Assets/Scripts/MoveRandomly.cs
+++ Assets/Scripts/MoveRandomly.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityEngine.AI;
 
 public class MoveRandomly : MonoBehaviour
 {
@@ -9,34 +11,43 @@ public class MoveRandomly : MonoBehaviour
     private float moveSpeed = 5.0f;
     
     private Vector3 destination;
+    public NavMeshAgent agent;
+    public ThirdPersonCharacter character;
 
     // Start is called before the first frame update
     void Start()
     {
         // Find a new random destination
         destination = FindNewDestination();
+        agent.updateRotation = false;
+        agent.SetDestination(destination);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * moveSpeed);
+        //transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * moveSpeed);
+        character.Move(agent.desiredVelocity, false, false);
 
-        if (transform.position == destination)
-        {
+        //if (transform.position == destination)
+        if (agent.remainingDistance <= agent.stoppingDistance)
+            {
             // Find a new random destination
+            character.Move(Vector3.zero, false, false);
             destination = FindNewDestination();
+            agent.SetDestination(destination);
         }
     }
 
     private Vector3 FindNewDestination()
     {
-        return new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(1.0f, 10.0f));
+        return new Vector3(Random.Range(-10.0f, 10.0f), transform.position.y, Random.Range(1.0f, 10.0f));
     }
-
+    /*
     private void LateUpdate()
     {
         transform.LookAt(2 * transform.position - Camera.main.transform.position);
         transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
     }
+    */
 }
